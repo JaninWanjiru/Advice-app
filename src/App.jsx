bffbff05e73e13c0c51e6e58c8488f27649df317
JaninWanjiru <janinndongo02@gmail.com>
2025-06-23 18:22:51 +0300

    add error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import { useState } from "react";
 function App() {
   const [advice, setAdvice] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function handleGetAdvice() {
     setLoading(true);
-    const response = await fetch(`https://api.adviceslip.com/advice`);
-    const advice = await response.json();
-    setAdvice(advice);
-    setLoading(false);
+    try {
+      const response = await fetch(`https://api.adviceslip.com/advice`);
+      const advice = await response.json();
+      setAdvice(advice);
+    } catch (e) {
+      setError("Something went wrong");
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
   }
   return (
     <>
       {advice && <h2>{advice.slip.advice}</h2>}
-      <button onClick={handleGetAdvice} disabled = {loading}>
+      {error && <h2>{error}</h2>}
+      <button onClick={handleGetAdvice} disabled={loading}>
         {loading ? "please wait..." : "Get random advice"}
       </button>
     </>
